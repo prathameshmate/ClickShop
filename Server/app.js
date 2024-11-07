76443ac29a6c53b631c117b8c3b775481b8007de
Prthmesh Mate <Prthmesh.Mate@winjittechnologiespvtltd.local>
2024-11-07 10:25:02 +0530

    adding server

---
+++ Server/app.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import morgan from 'morgan';
+import color from 'colors';
+import dotenv from 'dotenv';
+
+//importing all Environment varibale
+dotenv.config();
+
+//REST Object
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(morgan('dev'));
+
+//Routes
+app.get('/', (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: 'welcome to nodejs application',
+  });
+});
+
+//PORT
+const PORT = process.env.PORT || 5000;
+
+//Listing port number
+app.listen(PORT, () => {
+  console.log(`listing to the port :>> ${PORT}`.rainbow);
+});
