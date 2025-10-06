615bcd3eb523b8d934182816cea476f8ac4f80fc
prathameshmate <prathameshmate02@gmail.com>
2025-10-07 02:07:34 +0530

    adding deployed baseurl

--- src/Constant/Constant.ts
+++ src/Constant/Constant.ts
@@ -1,5 +1,6 @@
 export const CONS = {
-  baseURL: 'http://10.59.33.118:5000/',
+  // baseURL: 'http://10.59.33.118:5000/', // local
+  baseURL: 'https://dermatouch-assignment.vercel.app/', // vercel deployed backend
   errorMessage:
     'We are facing some technical difficulties please try after some time',
 };
